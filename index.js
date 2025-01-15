760dc45460e64ba2b3538fa00e227c5e2bb1877e
Josef <dev.josef1@gmail.com>
2025-01-15 05:55:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>AI Assistant Siri</title>
+      <title>S.I.R.I.U.S.</title>
       <style>
         * {
           margin: 0;
